b9e5f3ad051bd8e59535a680d6b087a589008aeb
jitendraparmar10 <jitparmar993@gmail.com>
2024-12-25 15:23:44 +0530

    Added tweet service with hashtag processing

---
+++ src/services/tweet-service.js
@@ -0,0 +1,37 @@
+const { TweetRepository, HashtagRepository } = require('../repository/index')
+
+class TweetService {
+    constructor(){
+        this.tweetRepository = new TweetRepository();
+        this.hashtagRepository = new HashtagRepository();
+    }
+
+    async create(data){
+        const content = data.content;
+        const tags = content.match(/#[a-zA-Z0-9_]+/g).map((tag)=>tag.substring(1)); //this regex extracts hashtags
+        const tweet = await this.tweetRepository.create(data);
+        let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
+        let titleOfPresentTags = alreadyPresentTags.map(tags => tags.title);
+        let newTags = tags.filter(tag =>!titleOfPresentTags.includes(tag));
+        newTags = newTags.map(tag => {
+            return {title: tag, tweets:[tweet.id]}
+        });
+        await this.hashtagRepository.bulkCreate(newTags);
+        alreadyPresentTags.forEach((tag)=>{
+            tag.tweets.push(tweet.id);
+            tag.save();
+        });
+        return tweet;
+    }
+}
+
+module.exports = TweetService;
+
+
+//todo create hashtags and add here
+        /**
+         * 1. bulkcreate in mongoose
+         * 2. filter title of hashtag based on multiple tags
+         * 3. how to add Tweet id inside all the hashtags
+         * 
+         */
