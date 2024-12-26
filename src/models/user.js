d1ae0703a7a367bae745d16672f865e72c59ad5d
jitendraparmar10 <jitparmar993@gmail.com>
2024-12-26 19:06:15 +0530

    Added like model and basic user model

---
+++ src/models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        require: true ,
+        unique:true
+    },
+    password: {
+        type:String,
+        require: true
+    },
+    name: {
+        type:String,
+        require:true
+    }
+},{timestamps: true});
+
+const User = new mongoose.model('User' , userSchema);
+
+export default User;
