db212cee5e545bb614604b89dce1ce8ebe2c9f8b
jitendraparmar10 <jitparmar993@gmail.com>
2024-12-26 22:11:15 +0530

    Added basic crud repo and extends it in like repo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,19 +1,13 @@
 import Tweet from '../models/tweet.js';
-
-class TweetRepository {
-
+import CrudRepository from './crud-repository.js';
+class TweetRepository extends CrudRepository {
+    constructor(){
+        super(Tweet);
+    }
+    
     async create(data){
         try {
             const tweet = await Tweet.create(data);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async get(id) {
-        try {
-            const tweet = await Tweet.findById(id);
             return tweet;
         } catch (error) {
             console.log(error);
@@ -29,15 +23,6 @@ class TweetRepository {
         }
     }
  
-    async destroy(id) {
-        try {
-            const tweet = await Tweet.findByIdAndRemove(id);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     async getAll(offset ,limit) {
         try {
             const tweet = await Tweet.find().skip(offset).limit(limit);
