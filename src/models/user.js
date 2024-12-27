23dd6de364699c59b3ca88a7b500a6ed573cd692
jitendraparmar10 <jitparmar993@gmail.com>
2024-12-28 01:45:24 +0530

    Added signup with encrypted password

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,14 @@ const userSchema = new mongoose.Schema({
         require:true
     }
 },{timestamps: true});
+
+userSchema.pre('save' , function (next){
+    const user = this;
+    const SALT = bcrypt.genSaltSync(9);
+    const encryptPassword = bcrypt.hashSync(user.password , SALT);
+    user.password = encryptPassword;
+    next();
+})
 
 const User = new mongoose.model('User' , userSchema);
 
